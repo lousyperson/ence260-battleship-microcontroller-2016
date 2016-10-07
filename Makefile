bb2de01066f2ec78d5e29e63746a9dbb7ef8db49
zyt10 <zyt10@uclive.ac.nz>
2016-10-08 01:35:36 +1300

    ships showing and can set initial positions

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 
 # Definitions.
 CC = avr-gcc
-CFLAGS = -mmcu=atmega32u2 -Os -Wall -Wstrict-prototypes -Wextra -g -I../../drivers/avr -I../../utils
+CFLAGS = -mmcu=atmega32u2 -Os -Wall -Wstrict-prototypes -Wextra -g -Iapis/drivers -Iapis/drivers/avr -Iapis/fonts -Iapis/utils
 OBJCOPY = avr-objcopy
 SIZE = avr-size
 DEL = rm
@@ -13,3 +13,62 @@ DEL = rm
 
 # Default target.
 all: game.out
+
+# Compile: create object files from C source files.
+game.o: game.c apis/drivers/avr/ir_uart.h apis/drivers/avr/system.h apis/drivers/display.h apis/drivers/navswitch.h apis/fonts/font5x7_1.h apis/utils/font.h apis/utils/tinygl.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+ir_uart.o: apis/drivers/avr/ir_uart.c apis/drivers/avr/ir_uart.h apis/drivers/avr/pio.h apis/drivers/avr/system.h apis/drivers/avr/timer0.h apis/drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+pio.o: apis/drivers/avr/pio.c apis/drivers/avr/pio.h apis/drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+prescale.o: apis/drivers/avr/prescale.c apis/drivers/avr/prescale.h apis/drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+system.o: apis/drivers/avr/system.c apis/drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+timer.o: apis/drivers/avr/timer.c apis/drivers/avr/system.h apis/drivers/avr/timer.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+timer0.o: apis/drivers/avr/timer0.c apis/drivers/avr/bits.h apis/drivers/avr/prescale.h apis/drivers/avr/system.h apis/drivers/avr/timer0.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+usart1.o: apis/drivers/avr/usart1.c apis/drivers/avr/system.h apis/drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+display.o: apis/drivers/display.c apis/drivers/avr/system.h apis/drivers/display.h apis/drivers/ledmat.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+ledmat.o: apis/drivers/ledmat.c apis/drivers/avr/pio.h apis/drivers/avr/system.h apis/drivers/ledmat.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+navswitch.o: apis/drivers/navswitch.c apis/drivers/avr/delay.h apis/drivers/avr/pio.h apis/drivers/avr/system.h apis/drivers/navswitch.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+font.o: apis/utils/font.c apis/drivers/avr/system.h apis/utils/font.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+pacer.o: apis/utils/pacer.c apis/drivers/avr/system.h apis/drivers/avr/timer.h apis/utils/pacer.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+tinygl.o: apis/utils/tinygl.c apis/drivers/avr/system.h apis/drivers/display.h apis/utils/font.h apis/utils/tinygl.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+# Link: create ELF output file from object files.
+game.out: game.o ir_uart.o pio.o prescale.o system.o timer.o timer0.o usart1.o display.o ledmat.o navswitch.o font.o tinygl.o
+	$(CC) $(CFLAGS) $^ -o $@ -lm
+	$(SIZE) $@
+
+# Target: clean project.
+.PHONY: clean
+clean:
+	-$(DEL) *.o *.out *.hex
+
+# Target: program project.
+.PHONY: program
+program: game.out
+	$(OBJCOPY) -O ihex game.out game.hex
+	dfu-programmer atmega32u2 erase; dfu-programmer atmega32u2 flash game.hex; dfu-programmer atmega32u2 start
---
+++ game.c
@@ -0,0 +1,205 @@
+#include "system.h"
+// #include "pacer.h"
+#include "navswitch.h"
+// #include "ir_uart.h"
+#include "tinygl.h"
+// #include "task.h"
+#include "../fonts/font5x7_1.h"
+
+#define PACER_RATE 500
+#define MESSAGE_RATE 10
+
+// Ship1 position
+static tinygl_point_t ship1_left_index;
+static tinygl_point_t ship1_mid_index;
+static tinygl_point_t ship1_right_index;
+// Ship2 position
+static tinygl_point_t ship2_left_index;
+static tinygl_point_t ship2_mid_index;
+static tinygl_point_t ship2_right_index;
+// Ship3 position
+static tinygl_point_t ship3_left_index;
+static tinygl_point_t ship3_mid_index;
+static tinygl_point_t ship3_right_index;
+// Number of ships
+static uint8_t no_ships = 0;
+
+// static uint8_t my_ship_count = 0;
+// static uint8_t ur_ship_count = 0;
+// static uint8_t p = 1;
+// static uint8_t map_view = 0;
+
+// static uint8_t ship_map[] =
+// {
+//     0x0, 0x0, 0x0, 0x0, 0x0
+// };
+//
+// static uint8_t hit_map[] =
+// {
+//     0x0, 0x0, 0x0, 0x0, 0x0
+// };
+
+static void planning_phase (void)
+{
+    uint8_t s_x = 2;
+    uint8_t s_mid_y = 3;
+
+    uint8_t set_ship = 0;
+    // uint8_t ship_placed = 0;
+
+    // uint8_t i;
+
+    // for (i = 0; i < 3; i++)
+    // {
+        // Initialise
+        tinygl_point_t ship_left_index = tinygl_point (s_x, s_mid_y+1);
+        tinygl_point_t ship_mid_index = tinygl_point (s_x, s_mid_y);
+        tinygl_point_t ship_right_index = tinygl_point (s_x, s_mid_y-1);
+        tinygl_pixel_set (ship_left_index, 1);
+        tinygl_pixel_set (ship_mid_index, 1);
+        tinygl_pixel_set (ship_right_index, 1);
+
+        // Move ship
+        while (1 && no_ships < 3)
+        {
+            navswitch_update ();
+            tinygl_update ();
+
+            if (navswitch_push_event_p (NAVSWITCH_NORTH) && s_mid_y > 1)
+            {
+                if (!set_ship)
+                {
+                    tinygl_pixel_set (ship_left_index, 0);
+                    tinygl_pixel_set (ship_mid_index, 0);
+                    tinygl_pixel_set (ship_right_index, 0);
+                }
+                s_mid_y--;
+                ship_left_index = tinygl_point (s_x, s_mid_y+1);
+                ship_mid_index = tinygl_point (s_x, s_mid_y);
+                ship_right_index = tinygl_point (s_x, s_mid_y-1);
+                tinygl_pixel_set (ship_left_index, 1);
+                tinygl_pixel_set (ship_mid_index, 1);
+                tinygl_pixel_set (ship_right_index, 1);
+                set_ship = 0;
+            }
+
+            if (navswitch_push_event_p (NAVSWITCH_SOUTH) && s_mid_y < 5)
+            {
+                if (!set_ship)
+                {
+                    tinygl_pixel_set (ship_left_index, 0);
+                    tinygl_pixel_set (ship_mid_index, 0);
+                    tinygl_pixel_set (ship_right_index, 0);
+                }
+                s_mid_y++;
+                ship_left_index = tinygl_point (s_x, s_mid_y+1);
+                ship_mid_index = tinygl_point (s_x, s_mid_y);
+                ship_right_index = tinygl_point (s_x, s_mid_y-1);
+                tinygl_pixel_set (ship_left_index, 1);
+                tinygl_pixel_set (ship_mid_index, 1);
+                tinygl_pixel_set (ship_right_index, 1);
+                set_ship = 0;
+            }
+
+            if (navswitch_push_event_p (NAVSWITCH_EAST) && s_x < 4)
+            {
+                if (!set_ship)
+                {
+                    tinygl_pixel_set (ship_left_index, 0);
+                    tinygl_pixel_set (ship_mid_index, 0);
+                    tinygl_pixel_set (ship_right_index, 0);
+                }
+                s_x++;
+                ship_left_index = tinygl_point (s_x, s_mid_y+1);
+                ship_mid_index = tinygl_point (s_x, s_mid_y);
+                ship_right_index = tinygl_point (s_x, s_mid_y-1);
+                tinygl_pixel_set (ship_left_index, 1);
+                tinygl_pixel_set (ship_mid_index, 1);
+                tinygl_pixel_set (ship_right_index, 1);
+                set_ship = 0;
+            }
+
+            if (navswitch_push_event_p (NAVSWITCH_WEST) && s_x > 0)
+            {
+                if (!set_ship)
+                {
+                    tinygl_pixel_set (ship_left_index, 0);
+                    tinygl_pixel_set (ship_mid_index, 0);
+                    tinygl_pixel_set (ship_right_index, 0);
+                }
+                s_x--;
+                ship_left_index = tinygl_point (s_x, s_mid_y+1);
+                ship_mid_index = tinygl_point (s_x, s_mid_y);
+                ship_right_index = tinygl_point (s_x, s_mid_y-1);
+                tinygl_pixel_set (ship_left_index, 1);
+                tinygl_pixel_set (ship_mid_index, 1);
+                tinygl_pixel_set (ship_right_index, 1);
+                set_ship = 0;
+            }
+
+            if (navswitch_push_event_p (NAVSWITCH_PUSH))
+            {
+                no_ships++;
+                set_ship = 1;
+                if (no_ships == 1)
+                {
+                    ship1_left_index = ship_left_index;
+                    ship1_mid_index = ship_mid_index;
+                    ship1_right_index = ship_right_index;
+                    tinygl_pixel_set (ship1_left_index, 1);
+                    tinygl_pixel_set (ship1_mid_index, 1);
+                    tinygl_pixel_set (ship1_right_index, 1);
+                }
+                else if (no_ships == 2)
+                {
+                    ship2_left_index = ship_left_index;
+                    ship2_mid_index = ship_mid_index;
+                    ship2_right_index = ship_right_index;
+                    tinygl_pixel_set (ship2_left_index, 1);
+                    tinygl_pixel_set (ship2_mid_index, 1);
+                    tinygl_pixel_set (ship2_right_index, 1);
+                }
+                else if (no_ships == 3)
+                {
+                    ship3_left_index = ship_left_index;
+                    ship3_mid_index = ship_mid_index;
+                    ship3_right_index = ship_right_index;
+                    tinygl_pixel_set (ship3_left_index, 1);
+                    tinygl_pixel_set (ship3_mid_index, 1);
+                    tinygl_pixel_set (ship3_right_index, 1);
+                }
+            }
+        }
+    // }
+}
+
+int main (void)
+{
+    system_init ();
+    tinygl_init (PACER_RATE);
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+    navswitch_init ();
+    // ir_uart_init ();
+
+    planning_phase();
+
+    while (1)
+    {
+        // tinygl_text ("DONE");
+        tinygl_update ();
+    }
+    // while (1)
+    // {
+    //     if (p == 1)
+    //     {
+    //         tinygl_update ();
+    //         navswitch_update ();
+    //     }
+    //     else
+    //     {
+    //
+    //     }
+    // }
+}
